390e0233e6f44b87052859e02a7bbf09017466cd
Antoine <ant.delalande@gmail.com>
2024-05-15 11:55:03 +0200

    added welcome route

--- server/index.js
+++ server/index.js
@@ -17,6 +17,11 @@ app
   .listen(port, () => {
     console.info(`Server is listening on port ${port}`);
   })
+
   .on("error", (err) => {
     console.error("Error:", err.message);
   });
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Wild Series !");
+});
