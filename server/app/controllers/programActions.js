2a39e72a5972eac8f402c0e4d87bfec05da5f503
Antoine <ant.delalande@gmail.com>
2024-05-15 15:58:20 +0200

    added router for categories

--- server/app/controllers/programActions.js
+++ server/app/controllers/programActions.js
@@ -28,9 +28,29 @@ const programs = [
 // Declare the action
 
 const browse = (req, res) => {
-  res.json(programs);
+  if (req.query.q != null) {
+    const filteredPrograms = programs.filter((program) =>
+      program.title.toLowerCase().includes(req.query.q.toLowerCase())
+    );
+    res.json(filteredPrograms);
+  } else res.json(programs);
+};
+
+const read = (req, res) => {
+    const parsedId = parseInt(req.params.id, 10);
+
+    const program = programs.find((p) => p.id === parsedId)
+
+    if (program != null) {
+        res.json(program)
+    } else {
+        res.sendStatus(404);
+    }
+
+    console.info(req.params);
+    res.send(`Hello Program ${req.params.id} !`)
 };
 
 // Export it to import it somewhere else
 
-module.exports = { browse };
+module.exports = { browse, read };
--- server/app/routers/api/categories/router.js
+++ server/app/routers/api/categories/router.js
@@ -7,10 +7,12 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Import item-related actions
-const { browse, read } = require("../../../controllers/programActions");
+const { browse, read } = require("../../../controllers/categoryActions");
 
 // Route to get a list of items
 router.get("/", browse);
+
+// Route to get a specific item by ID
 router.get("/:id", read);
 
 /* ************************************************************************* */
